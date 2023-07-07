6c49695860b5b47610b2c05f98e66cd4877527ac
rifatahmed787 <mdrifatahmed787@gmail.com>
2023-07-08 00:38:02 +0600

    done

--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -4,11 +4,6 @@ type IApiReponse<T> = {
   statusCode: number;
   success: boolean;
   message?: string | null;
-  meta?: {
-    page: number;
-    limit: number;
-    total: number;
-  };
   data?: T | null;
 };
 
@@ -17,7 +12,6 @@ const sendResponse = <T>(res: Response, data: IApiReponse<T>): void => {
     statusCode: data.statusCode,
     success: data.success,
     message: data.message || null,
-    meta: data.meta || null || undefined,
     data: data.data || null || undefined,
   };
 
